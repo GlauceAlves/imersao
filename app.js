822e673369c19bba9c539188c2881d7f3d204b13
Glauce Alves <glaucemail@gmail.com>
2024-09-06 19:17:02 -0300

    aula05   - imersão Alura

--- app.js
+++ app.js
@@ -2,29 +2,58 @@
 //   Realiza uma pesquisa nos dados e atualiza a seção de resultados na página.
 
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-  
-    // **Para fins de depuração:** Exibe o elemento section no console do navegador
-    console.log(section);
-  
-    // Inicializa uma string vazia para armazenar os resultados da pesquisa
-    let resultados = "";
-  
-    // Itera sobre cada item (dado) no array de dados
-    for (let dado of dados) {
+  // Obtém a seção HTML onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+  // **Para fins de depuração:** Exibe o elemento section no console do navegador
+  console.log(section);
+
+  // **Para fins de depuração:** Exibe a informação no console do navegador
+  console.log(campoPesquisa);
+
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+ 
+  if (campoPesquisa == "") {
+    // alert("Informe o que desaja pesquisar") 
+    section.innerHTML = "<p>Não foi encontrados resultados para pesquisa!</p>"
+    return
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  // inicializa a string vazia para armazenar os resultados 
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
+
+
+  // Itera sobre cada item (dado) no array de dados
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    // console.log(dado.titulo.includes(campoPesquisa))
+    // verifica dados dentro da condição de pesquisa
+    if (dado.titulo.includes(campoPesquisa) || dado.descricao.includes(campoPesquisa) || dado.tags.includes(campoPesquisa)){
       // Constrói o HTML para cada resultado, formatando o título, descrição e link
       resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Mais informações</a>
-        </div>
-      `;
+   <div class="item-resultado">
+     <h2>
+       <a href="#" target="_blank">${dado.titulo}</a>
+     </h2>
+     <p class="descricao-meta">${dado.descricao}</p>
+     <a href="${dado.link}" target="_blank">Mais informações</a>
+   </div>
+ `;
     }
-  
-    // Adiciona os resultados construídos ao conteúdo HTML da seção
-    section.innerHTML += resultados;
+
   }
+  
+  if (!resultados){
+    resultados = "<p>Nada foi encontrado.</p>"
+  }
+  // Adiciona os resultados construídos ao conteúdo HTML da seção
+  section.innerHTML += resultados;
+}
--- dados.js
+++ dados.js
@@ -2,21 +2,25 @@ let dados = [
     {
         titulo: "Cantiga de roda",
         descricao: "Conhecidas como cirandas ou brincadeiras de roda. As crianças formam uma roda de mãos dadas e cantam melodias folclóricas, podendo executar ou não coreografias acerca da letra da música.",
-        link: "https://pt.wikipedia.org/wiki/Cantiga_de_roda"
+        link: "https://pt.wikipedia.org/wiki/Cantiga_de_roda",
+        tags: "cantar melodias danças dancas"
     },
     {
         titulo: "Pular corda",
         descricao: "Consiste não apenas em pular corda, mas sim executar uma série de saltos, acrobacias, manejos com a corda, buscando a sincronia dos saltadores com uma música em execução.",
-        link: "https://pt.wikipedia.org/wiki/Pular_corda"
+        link: "https://pt.wikipedia.org/wiki/Pular_corda",
+        tags: "cantar melodias danças dancas"
     },
     {
         titulo: "Amarelinha",
         descricao: "O jogo consiste em saltar num pé sobre oito quadrados gizados ou riscado no chão, que compõem uma figura geométrica, salvo sobre aquele onde cair a pedra ou malha, que é lançada pelos jogadores antes de começarem a jogar.",
-        link: "https://pt.wikipedia.org/wiki/Amarelinha"
+        link: "https://pt.wikipedia.org/wiki/Amarelinha",
+        tags: "cantar melodias danças dancas"
     },
     {
         titulo: "Pau de sebo",
         descricao: "consiste em tentar subir em um tronco reto e liso previamente banhado de sebo ou graxa, ou qualquer outra substância gordurosa, para tentar apanhar um prêmio que se encontra em seu topo..",
-        link: "https://pt.wikipedia.org/wiki/Pau_de_sebo"
+        link: "https://pt.wikipedia.org/wiki/Pau_de_sebo",
+        tags: "cantar melodias danças"
     }
 ];
